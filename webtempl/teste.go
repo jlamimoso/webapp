4dad73dee9bade2c5b213fb1212d81f357a77703
jlamimoso <jlamimoso@yahoo.com.br>
2015-01-29 17:54:42 -0200

    Alt 8
    Desc 8

--- webtempl/teste.go
+++ webtempl/teste.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/context"
 	"github.com/julienschmidt/httprouter"
+	"github.com/justinas/alice"
 	"html/template"
 	"log"
 	"net/http"
+	"time"
 )
 
 var templates = template.Must(template.ParseFiles("edit.html", "view.html", "index.html", "templates/home.html"))
@@ -45,30 +48,59 @@ func (x tipox) teste(v int) int {
 }
 
 /*
-var routerr *httprouter.Router
-
-func NewRouter() *router {
-	router.
+type hr struct {
+	*httprouter.Router
 }
 
-func (r *router) get(path string, h http.Handler) {
-	//r.GET(path, h)
-	r
+func NewRouter() *hr {
+	return &hr{httprouter.New()}
 }
 */
+
+type hr *httprouter.Router
+
+func (r *hr) get(path string, h http.Handler) {
+	r.GET(path, wrapHandler(h))
+}
+
+func wrapHandler(h http.Handler) httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		context.Set(r, "params", p)
+		h.ServeHTTP(w, r)
+	}
+}
+
+func loggingHandler(next http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		t1 := time.Now()
+		next.ServeHTTP(w, r)
+		t2 := time.Now()
+		log.Printf("log -> [%s] %q %v\n", r.Method, r.URL.String(), t2.Sub(t1))
+	}
+	return http.HandlerFunc(fn)
+}
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "You are on the about page 1.")
+}
+
 func main() {
-	//	r := NewRouter()
+	commonHandler := alice.New(loggingHandler)
+	//r := NewRouter()
+	r := hr(httprouter.New())
+	r.get("/about", commonHandler.ThenFunc(aboutHandler))
+	/*
+		router := httprouter.New()
+		y := tipox(mostrarX)
+		fmt.Printf("valor do tipo x %d", y.teste(2))
+		router.ServeFiles("/css/*filepath", http.Dir("./css/"))
+		router.ServeFiles("/js/*filepath", http.Dir("./js/"))
+		router.GET("/", Index)
+		//router.GET("/css/", css)
+		//router.GET("/js/", js)
+		router.GET("/hello/:name", Hello)
 
-	router := httprouter.New()
-
-	y := tipox(mostrarX)
-	fmt.Printf("valor do tipo x %d", y.teste(2))
-	router.ServeFiles("/css/*filepath", http.Dir("./css/"))
-	router.ServeFiles("/js/*filepath", http.Dir("./js/"))
-	router.GET("/", Index)
-	//router.GET("/css/", css)
-	//router.GET("/js/", js)
-	router.GET("/hello/:name", Hello)
-
-	log.Fatal(http.ListenAndServe(":8080", router))
+		log.Fatal(http.ListenAndServe(":8080", router))
+	*/
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
